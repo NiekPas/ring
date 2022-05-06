2c93004217181f9573e200679c8728e787ed95e9
Niek van de Pas <niek_pas@live.nl>
2022-05-06 10:47:19 +0200

    test: initial unit tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,35 @@
 import Ring from "../src";
 
-describe("Ring test", () => {
+describe("Ring instantiation", () => {
     test("Should instantiate an empty ring", () => {
         expect(new Ring()).toStrictEqual(new Ring());
         expect(new Ring().toArray()).toStrictEqual([]);
-    })
+    });
+
+    test("Should instantiate a ring with elements", () => {
+        const r = new Ring([1,2,3,4]);
+
+        expect(r).toStrictEqual(new Ring([1,2,3,4]));
+        expect(r.toArray()).toStrictEqual([1,2,3,4]);
+    });
+
+    test("Should instantiate a ring with elements and focus", () => {
+        const r = new Ring([1,2,3,4], 2);
+
+        expect(r.toArray()).toStrictEqual([3,4,1,2]);
+        expect(r.focus()).toBe(3);
+    });
 });
+
+describe("Retrieving focus", () => {
+    test("should retrieve default focus", () => {
+        expect(new Ring([0,1,2]).focus()).toBe(0);
+    });
+
+    test("should not retrieve focus from an empty ring", () => {
+        expect(new Ring().focus).toThrow(TypeError);
+    });
+});
+
+describe("Inserting elements", () => {})
+describe("Rotations", () => {});
