5cd954a233419f7b37d5549df42def8f703b51aa
Niek van de Pas <niek_pas@live.nl>
2022-05-09 20:45:00 +0200

    test: fix bug in test cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -19,13 +19,13 @@ describe("Ring instantiation", () => {
         const r = new Ring([1,2,3,4], 2);
 
         expect(r.toArray()).toStrictEqual([3,4,1,2]);
-        expect(r.focus()).toBe(3);
+        expect(r.focus).toBe(3);
     });
 });
 
 describe("Retrieving focus", () => {
     test("should retrieve default focus", () => {
-        expect(new Ring([0,1,2]).focus()).toBe(0);
+        expect(new Ring([0,1,2]).focus).toBe(0);
     });
 
     test("should not retrieve focus from an empty ring", () => {
@@ -38,26 +38,26 @@ describe("Rotations", () => {
     const r = new Ring(testNumbers);
 
     test("should perform a single clockwise rotation", () => {
-        expect(r.rotate(1).focus()).toEqual(2);
+        expect(r.rotate(1).focus).toEqual(2);
     });
 
     test("should perform multiple clockwise rotations", () => {
-        expect(r.rotate(4).focus()).toEqual(5);
+        expect(r.rotate(4).focus).toEqual(5);
     });
 
     test("should perform a lapping clockwise rotation", () => {
-        expect(r.rotate(27).focus()).toEqual(4);
+        expect(r.rotate(27).focus).toEqual(4);
     });
 
     test("should perform a single counterclockwise rotation", () => {
-        expect(r.rotate(-1).focus()).toEqual(6);
+        expect(r.rotate(-1).focus).toEqual(6);
     });
 
     test("should perform multiple counterclockwise rotations", () => {
-        expect(r.rotate(-4).focus()).toEqual(3);
+        expect(r.rotate(-4).focus).toEqual(3);
     });
 
     test("should perform a lapping counterclockwise rotation", () => {
-        expect(r.rotate(-27).focus()).toEqual(4);
+        expect(r.rotate(-27).focus).toEqual(4);
     });
 });
