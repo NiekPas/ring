e823bc6613f55faaaaa892a45ce12128235b745b
Niek van de Pas <niek_pas@live.nl>
2022-05-10 17:19:03 +0200

    chore: formatting

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
 import Ring from "../src";
 
-const testNumbers = [1,2,3,4,5,6]
+const testNumbers = [1, 2, 3, 4, 5, 6];
+const testStrings = ["a", "b", "c", "d", "e", "f", "g"];
 
 describe("Ring instantiation", () => {
     test("Should instantiate an empty ring", () => {
@@ -9,23 +10,23 @@ describe("Ring instantiation", () => {
     });
 
     test("Should instantiate a ring with elements", () => {
-        const r = new Ring([1,2,3,4]);
+        const r = new Ring([1, 2, 3, 4]);
 
         expect(r).toStrictEqual(new Ring([1,2,3,4]));
         expect(r.toArray()).toStrictEqual([1,2,3,4]);
     });
 
     test("Should instantiate a ring with elements and focus", () => {
-        const r = new Ring([1,2,3,4], 2);
+        const r = new Ring([1, 2, 3, 4], 2);
 
-        expect(r.toArray()).toStrictEqual([3,4,1,2]);
+        expect(r.toArray()).toStrictEqual([3, 4, 1, 2]);
         expect(r.focus).toBe(3);
     });
 });
 
 describe("Retrieving focus", () => {
     test("should retrieve default focus", () => {
-        expect(new Ring([0,1,2]).focus).toBe(0);
+        expect(new Ring([0, 1, 2]).focus).toBe(0);
     });
 
     test("should not retrieve focus from an empty ring", () => {
@@ -33,7 +34,7 @@ describe("Retrieving focus", () => {
     });
 });
 
-describe("Inserting elements", () => {}); // TODO
+describe("Inserting elements", () => { }); // TODO
 describe("Rotations", () => {
     const r = new Ring(testNumbers);
 
